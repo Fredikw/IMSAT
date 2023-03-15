21c02b50a021ca8a3630cec5a58c846a3f4ea79b
Fred Kwizera <fredikw@ntnu.no>
2023-03-15 11:38:42 +0100

    perturbed function sceleton

--- code/IIC.py
+++ code/IIC.py
@@ -18,6 +18,15 @@ Setting hyperparameters for the IMSAT algorithm
 
 """
 
+#TODO
+def perturbation(x: torch.Tensor) -> torch.Tensor:
+
+    # Generate random unit tensor for perturbation direction
+    d = torch.randn_like(x)
+    d = F.normalize(d, p=2, dim=1)
+
+    return x + d
+
 def invariant_information_clustering(model, x: torch.Tensor, y: torch.Tensor, C: int = 10, EPS: float=float_info.epsilon) -> float:
     """
     Calculate the invariant information clustering (IIC) loss.
@@ -31,17 +40,13 @@ def invariant_information_clustering(model, x: torch.Tensor, y: torch.Tensor, C:
     Returns:
         float: Invariant Information Clustering (IIC) loss.
     """
-    # TODO rewirte
-    # Generate random unit tensor for perturbation direction
-    d = torch.randn_like(x, requires_grad=True)
-    d = F.normalize(d, p=2, dim=1)
     
-    # TODO rewrite
-    # Apply perturbation
-    yt = model(x + d)
+    # Compute representation of perturbed input.
+    yt = model(perturbation(x))
 
     # Compute the joint probability matrix, symmetrize and normalize matrix
-    P = (y.unsqueeze(2) * yt.unsqueeze(1)).sum(dim=0)
+    # P = (y.unsqueeze(2) * yt.unsqueeze(1)).sum(dim=0)
+    P = torch.einsum('ik,jk->ij', y, yt)
     P = ((P + P.t()) / 2) / P.sum()
     P.clamp_(min=EPS)
     
